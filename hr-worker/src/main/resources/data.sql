15e936e07c1681daa86d95075b2038f2731b56c5
Rodrigo Valença <rodrigo.rvcf@gmail.com>
2022-02-19 16:40:27 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (daily_Income, name) VALUES (200.0, 'Bob');
+INSERT INTO tb_worker (daily_Income, name) VALUES (300.0, 'Maria');
+INSERT INTO tb_worker (daily_Income, name) VALUES (250.0, 'Alex');
