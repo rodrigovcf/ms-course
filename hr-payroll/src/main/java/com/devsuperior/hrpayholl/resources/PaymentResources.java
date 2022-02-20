32cbd0245a77573abeeb9008eaebd40a5288254b
Rodrigo Valença <rodrigo.rvcf@gmail.com>
2022-02-20 16:32:39 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayholl/resources/PaymentResources.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayholl/resources/PaymentResources.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hrpayholl.sources;
+package com.devsuperior.hrpayholl.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
